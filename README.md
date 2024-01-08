2b172016924a09e2073f568aecc22a9ce1550d66
Lucas Saliés Brum <sistematico@gmail.com>
2024-01-08 06:01:08 -0400

    Added base

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Vue 3 + TypeScript + Vite
+# Paxá WebDev
+
+[![Deploy](https://github.com/sistematico/paxa.dev/actions/workflows/cd.yml/badge.svg)](https://github.com/sistematico/paxa.dev/actions/workflows/cd.yml)
 
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
---
+++ src/app.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Icon from '@/components/icon.vue'
+</script>
+<template>
+  <div class="container">
+    <div class="avatar">
+      <a href="#">
+        <img src="/img/pasha.png" alt="Paxá" />
+      </a>
+    </div>
+    <div class="content">
+      <h1>Paxá</h1>
+      <h2>Desenvolvimento Web</h2>
+      <p>
+        <span>
+          <a href="https://x.com/paxadev" target="_blank">
+            <Icon name="twitter" />
+          </a>
+        </span>
+        <span>
+          <a href="https://www.linkedin.com/company/paxadev" target="_blank">
+            <Icon name="linkedin" />
+          </a>
+        </span>
+        <span>
+          <a href="https://github.com/Paxa-Web-Dev" target="_blank">
+            <Icon name="github" />
+          </a>
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
